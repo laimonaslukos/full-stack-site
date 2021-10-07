0c38e76342551a9308995b62f74180ede03807d1
„Laimonas <laimonas.lukosevicius@gmail.com>
2021-10-07 10:31:25 +0300

    Add HomePage, Hello World

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => (
+  <>
+  <h1>Hello World</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nobis possimus dicta, inventore labore quasi soluta cum velit dolorum corporis?</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nobis possimus dicta, inventore labore quasi soluta cum velit dolorum corporis?</p>
+  </>
+)
+
+export default HomePage
